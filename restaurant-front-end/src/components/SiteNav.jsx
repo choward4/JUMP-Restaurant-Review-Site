201754e54b53e36c782f9de434bd817b2f4b7d7c
choward4 <chrishoward1337@gmail.com>
2021-07-05 17:05:25 -0500

    commit

--- restaurant-front-end/src/components/SiteNav.jsx
+++ restaurant-front-end/src/components/SiteNav.jsx
@@ -1,31 +1,41 @@
 import React, {useState} from 'react';
 import { Navbar, Nav, Form, FormControl, Button, Row } from 'react-bootstrap';
 
-
 const SiteNav = (props) => {
     // State contains values for search bar to pass back up to App
     const [searchValue, setSearchValue] = useState('');
-
+  
     // State changes logged to pass back up to app when search button is clicked
     const handleSearch = (evt) => {
-    evt.preventDefault();
-    props.search(searchValue, true);
+      evt.preventDefault();
+      props.search(searchValue, true);
     }
-
+  
     console.log(searchValue);
-
+  
     return (
-        <Navbar bg="dark" variant="dark" sticky='top'>
-            <Navbar.Brand href="/">Restaurant</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-
-            </Nav>
-
-
-        </Navbar>
+      <Navbar bg="dark" variant="dark" sticky='top'>
+        <Navbar.Brand href="/">ICan'tBelieveIt'sNotYelp</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+        </Nav>
+        <a href="/results">
+            <Button variant='primary'>
+                Show All
+            </Button>
+        </a>
+        
+        <Form inline onSubmit={handleSearch}>
+          <FormControl  type="text" placeholder="Search Restaurants" className="mr-sm-2" onChange={e=>setSearchValue(e.target.value)} />
+          <Button variant="outline-info" type='submit'>Search</Button>
+        </Form>
+        <Nav className="mr-auto">
+          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link href="/signup">SignUp</Nav.Link>
+        </Nav>
+        
+      </Navbar>
     );
-
-}
-
-export default SiteNav;
+  }
+  
+  export default SiteNav;
